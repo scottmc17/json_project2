588bea340cac300024bb0a92c2b9e423a4807ccc
McIntyre <Scott_McIntyre1@baylor.edu>
2020-09-27 20:41:35 -0500

    JSON changes

---
+++ september_wildfires.py
@@ -0,0 +1,47 @@
+## JSON PROJECT 
+import json
+
+sept_in_file = open('US_fires_9_1.json', 'r')
+
+sept_out_file = open('bspet_fire_data.json', 'w')
+
+sept_data = json.load(sept_in_file)
+
+json.dump(sept_data, sept_out_file, indent = 4)
+
+blons, blats, bbrightness = [], [], []
+
+
+for x in sept_data:
+    lon = x['longitude']
+    lat = x['latitude']
+    bright = x['brightness']
+    blons.append(lon)
+    blats.append(lat)
+    if bright > 450:
+        bbrightness.append(bright)
+
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [{
+    'type' : 'scattergeo',
+    'lon' : blons,
+    'lat' : blats,
+    'marker' : {
+        'size' : [.05 * bright for bright in bbrightness],
+        'color' : bbrightness,
+        'colorscale' : 'Vilridis',
+        'reversescale' : True,
+        'colorbar' : {'title' : 'Brightness'}
+    },
+}]
+
+my_layout = Layout(title= 'Fires')
+
+fig = {'data' : data, 'layout' : my_layout}
+
+offline.plot(fig, filename = 'beg_sept_fires.html')
+
+#display fig
